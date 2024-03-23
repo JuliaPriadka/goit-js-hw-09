55cf7475e9ce4e59f0af44c20cff4d002c182b5c
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-03-24 00:51:09 +0200

    Update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,27 @@
-console.log("Form");
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', onInputForm);
+form.addEventListener("submit", onFormSubmit);
+
+const formNewState = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+form.elements.email.value = formNewState.email ?? '';
+
+form.elements.message.value = formNewState.message ?? '';
+
+function onInputForm(event) {
+  let userEmail = event.currentTarget.elements.email.value;
+  let userMessage = event.currentTarget.elements.message.value;
+
+  const formState = {
+    email: userEmail,
+    message: userMessage,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+}
